5e4888f75347767b1760a699df2ddd686a64d0de
seimou <seimou@techsec.com.br>
2019-09-30 13:49:14 -0400

    Início da Implementação

--- module/Cadastro/Module.php
+++ module/Cadastro/Module.php
@@ -1,8 +1,8 @@
 <?php
 namespace Album;
 
-use Album\Model\Album;
-use Album\Model\AlbumTable;
+use Cadastro\Model\Cadastro;
+use Cadastro\Model\CadastroTable;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
 
@@ -23,19 +23,19 @@ class Module
     {
         return array(
             'factories' => array(
-                'Album\Model\AlbumTable' => function($sm) 
+                'Cadastro\Model\CadastroTable' => function($sm) 
                 {
-                    $tableGateway = $sm->get('AlbumTableGateway');
-                    $table = new AlbumTable($tableGateway);
+                    $tableGateway = $sm->get('CadastroTableGateway');
+                    $table = new CadastroTable($tableGateway);
                     return $table;
                 },
                     
-                'AlbumTableGateway' => function ($sm) {
+                'CadastroTableGateway' => function ($sm) {
                     $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
                     $resultSetPrototype = new ResultSet();
                     $resultSetPrototype->setArrayObjectPrototype(new Album());
                 
-                    return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+                    return new TableGateway('cadastro', $dbAdapter, null, $resultSetPrototype);
                 },
             ),
         );
