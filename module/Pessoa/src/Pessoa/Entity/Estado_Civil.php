7c5615fb51f07ebe720b582314def7c45d5a2f82
seimou <seimou@techsec.com.br>
2019-10-02 11:14:48 -0400

    Inserção de novos arquivos!

---
+++ module/Pessoa/src/Pessoa/Entity/Estado_Civil.php
@@ -0,0 +1,116 @@
+<?php
+namespace Pessoa\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface; 
+
+/**
+ *
+ * @ORM\Entity 
+ * @ORM\Table(name="estadocivil") 
+ */
+
+class Estado_Civl implements InputFilterAwareInterface 
+{   
+     /**
+     * @ORM\Id
+     * @ORM\Column(type="integer");
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;       
+    
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $descricao;  
+    
+     /**
+     * Populate from an array.
+     *
+     * @param array $data
+     */
+    public function exchangeArray($data = array()) 
+    {
+        $this->id = $data['id'];
+        $this->descricao = $data['descricao'];                
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) 
+        {
+            $inputFilter = new InputFilter();
+ 
+            $inputFilter->add(array(
+                'name'     => 'id',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            ));     
+                        
+            $inputFilter->add(array(
+                'name'     => 'descricao',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));            
+            
+            $this->inputFilter = $inputFilter;
+        }
+ 
+        return $this->inputFilter;
+    }
+
+    /**
+     * Magic getter to expose protected properties.
+     *
+     * @param string $property
+     * @return mixed
+     */
+    public function __get($property) 
+    {
+        return $this->$property;
+    }
+
+    /**
+     * Magic setter to save protected properties.
+     *
+     * @param string $property
+     * @param mixed $value
+     */
+    public function __set($property, $value) 
+    {
+        $this->$property = $value;
+    }
+
+    /**
+     * Convert the object to an array.
+     *
+     * @return array
+     */
+    public function getArrayCopy() 
+    {
+        return get_object_vars($this);
+    }
+}
---
+++ module/Pessoa/src/Pessoa/Entity/PessoaJuridica.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Pessoa\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface; 
+
+/**
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="pessoajuridica") 
+ */
+
+Class PessoaJuridica extends Pessoa
+{
+     /**
+     * @var InputFilter
+     */
+    protected $inputFilter;
+
+    /**
+     * @var \Pessoa
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="NONE")
+     * @ORM\OneToOne(targetEntity="Pessoa")
+     * @ORM\JoinColumns({
+     *  @ORM\JoinColumn(name="id", referencedColumnName="id")
+     * })     
+     */        
+    protected $id;
+    
+     /**
+     * @ORM\Column(type="string")
+     */
+    protected $cnpj;
+    
+     /**
+     * Populate from an array.
+     *
+     * @param array $data
+     */
+    public function exchangeArray($data = array()) 
+    {
+        $this->id = $data['id'];
+        $this->situacao = $data['situacao'];                
+        $this->email = $data['email'];
+        $this->telefone = $data['telefone'];
+        $this->nome_fantasia = $data['nome_fantasia'];
+        $this->razaosocial = $data['razaosocial'];        
+        $this->id1 = $data['id1'];        
+        $this->cnpj = $data['cnpj'];
+    }              
+    
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) 
+        {
+            $inputFilter = new InputFilter();
+ 
+            $inputFilter->add(array(
+                'name'     => 'id',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            ));     
+            
+            $inputFilter->add(array(
+                'name'     => 'situacao',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            ));                     
+            
+            $inputFilter->add(array(
+                'name'     => 'email',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+ 
+            $inputFilter->add(array(
+                'name'     => 'telefone',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+ 
+            $inputFilter->add(array(
+                'name'     => 'nome_fantasia',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+            
+            $inputFilter->add(array(
+                'name'     => 'razaosocial',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+            
+            $inputFilter->add(array(
+                'name'     => 'id1',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            ));
+                        
+            $inputFilter->add(array(
+                'name'     => 'cnpj',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            ));
+            
+            $this->inputFilter = $inputFilter;
+        }
+ 
+        return $this->inputFilter;
+    }
+}
